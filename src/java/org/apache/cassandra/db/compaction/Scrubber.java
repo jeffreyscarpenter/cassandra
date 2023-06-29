msmygit/vscassandra

STAR-748: Fix Scrubber so that it can work in case of broken index (#193)
Fix scrubber so that it can work with broken indexes.

If the index is broken, but the data file is ok, we can continue scrubbing and the index will be rebuilt. With this patch it will work even if we cannot access the initial position in the index. Also, the corrupted file will be obsoleted before finishing the rewriter so that it will not be attempted to move starts (and thus fail at the end as move starts requires correct indexes).

There are also some explanations added to how canonical set of sstables is constructed and why

Co-authored-by: Branimir Lambov <branimir.lambov@datastax.com>

src/java/org/apache/cassandra/db/compaction/Scrubber.java
@@ -167,12 +167,23 @@ public class Scrubber implements Closeable
         try (SSTableRewriter writer = SSTableRewriter.construct(cfs, transaction, false, sstable.maxDataAge);
              Refs<SSTableReader> refs = Refs.ref(Collections.singleton(sstable)))
         {
-            nextIndexKey = indexAvailable() ? ByteBufferUtil.readWithShortLength(indexFile) : null;
-            if (indexAvailable())
+            try
             {
-                // throw away variable so we don't have a side effect in the assert
-                long firstRowPositionFromIndex = rowIndexEntrySerializer.deserializePositionAndSkip(indexFile);
-                assert firstRowPositionFromIndex == 0 : firstRowPositionFromIndex;
+                nextIndexKey = indexAvailable() ? ByteBufferUtil.readWithShortLength(indexFile) : null;
+                if (indexAvailable())
+                {
+                    // throw away variable so we don't have a side effect in the assert
+                    long firstRowPositionFromIndex = rowIndexEntrySerializer.deserializePositionAndSkip(indexFile);
+                    assert firstRowPositionFromIndex == 0 : firstRowPositionFromIndex;
+                }
+            }
+            catch (Throwable ex)
+            {
+                throwIfFatal(ex);
+                nextIndexKey = null;
+                nextRowPositionFromIndex = dataFile.length();
+                if (indexFile != null)
+                    indexFile.seek(indexFile.length());
             }
 
             StatsMetadata metadata = sstable.getSSTableMetadata();
@@ -199,17 +210,21 @@ public class Scrubber implements Closeable
                     // check for null key below
                 }
 
-                updateIndexKey();
-
-                long dataStart = dataFile.getFilePointer();
-
                 long dataStartFromIndex = -1;
                 long dataSizeFromIndex = -1;
-                if (currentIndexKey != null)
+
+                updateIndexKey();
+
+                if (indexAvailable())
                 {
-                    dataStartFromIndex = currentRowPositionFromIndex + 2 + currentIndexKey.remaining();
-                    dataSizeFromIndex = nextRowPositionFromIndex - dataStartFromIndex;
+                    if (currentIndexKey != null)
+                    {
+                        dataStartFromIndex = currentRowPositionFromIndex + 2 + currentIndexKey.remaining();
+                        dataSizeFromIndex = nextRowPositionFromIndex - dataStartFromIndex;
+                    }
                 }
+
+                long dataStart = dataFile.getFilePointer();
 
                 // avoid an NPE if key is null
                 String keyName = key == null ? "(unreadable key)" : ByteBufferUtil.bytesToHex(key.getKey());
@@ -295,12 +310,9 @@ public class Scrubber implements Closeable
             }
 
             // finish obsoletes the old sstable
+            transaction.obsoleteOriginals();
             finished.addAll(writer.setRepairedAt(badRows > 0 ? ActiveRepairService.UNREPAIRED_SSTABLE : sstable.getSSTableMetadata().repairedAt).finish());
             completed = true;
-        }
-        catch (IOException e)
-        {
-            throw Throwables.propagate(e);
         }
         finally
         {
@@ -379,8 +391,8 @@ public class Scrubber implements Closeable
             nextIndexKey = !indexAvailable() ? null : ByteBufferUtil.readWithShortLength(indexFile);
 
             nextRowPositionFromIndex = !indexAvailable()
-                    ? dataFile.length()
-                    : rowIndexEntrySerializer.deserializePositionAndSkip(indexFile);
+                                       ? dataFile.length()
+                                       : rowIndexEntrySerializer.deserializePositionAndSkip(indexFile);
         }
         catch (Throwable th)
         {
@@ -388,6 +400,8 @@ public class Scrubber implements Closeable
             outputHandler.warn("Error reading index file", th);
             nextIndexKey = null;
             nextRowPositionFromIndex = dataFile.length();
+            if (indexFile != null)
+                indexFile.seek(indexFile.length());
         }
     }
 

jeffreyscarpenter/cassandra

(cherry picked from commit c0be3ae5cdd89fae0afcbb277322a466ece4de00)
(cherry picked from commit b777dbebfbdf3006141da5825acd0a0c1b542840)
(cherry picked from commit 7bd43ecff6b50919a492db490f4821a7a34ed064)
(cherry picked from commit d5321b700da844372d4a13bd959df74ce71534dd)
(cherry picked from commit e5f81f93d96a1e4ac8b83fdd647bc864933cab43)
(cherry picked from commit 334a8fbc51ccb3c4dabf9ddb3e432dba7bd62b28)

src/java/org/apache/cassandra/db/compaction/Scrubber.java
@@ -175,12 +175,23 @@ public class Scrubber implements Closeable
         try (SSTableRewriter writer = SSTableRewriter.construct(cfs, transaction, false, sstable.maxDataAge);
              Refs<SSTableReader> refs = Refs.ref(Collections.singleton(sstable)))
         {
-            nextIndexKey = indexAvailable() ? ByteBufferUtil.readWithShortLength(indexFile) : null;
-            if (indexAvailable())
+            try
             {
-                // throw away variable so we don't have a side effect in the assert
-                long firstRowPositionFromIndex = rowIndexEntrySerializer.deserializePositionAndSkip(indexFile);
-                assert firstRowPositionFromIndex == 0 : firstRowPositionFromIndex;
+                nextIndexKey = indexAvailable() ? ByteBufferUtil.readWithShortLength(indexFile) : null;
+                if (indexAvailable())
+                {
+                    // throw away variable so we don't have a side effect in the assert
+                    long firstRowPositionFromIndex = rowIndexEntrySerializer.deserializePositionAndSkip(indexFile);
+                    assert firstRowPositionFromIndex == 0 : firstRowPositionFromIndex;
+                }
+            }
+            catch (Throwable ex)
+            {
+                throwIfFatal(ex);
+                nextIndexKey = null;
+                nextPartitionPositionFromIndex = dataFile.length();
+                if (indexFile != null)
+                    indexFile.seek(indexFile.length());
             }
 
             StatsMetadata metadata = sstable.getSSTableMetadata();
@@ -210,17 +221,21 @@ public class Scrubber implements Closeable
                     // check for null key below
                 }
 
-                updateIndexKey();
-
-                long dataStart = dataFile.getFilePointer();
-
                 long dataStartFromIndex = -1;
                 long dataSizeFromIndex = -1;
-                if (currentIndexKey != null)
+
+                updateIndexKey();
+
+                if (indexAvailable())
                 {
-                    dataStartFromIndex = currentPartitionPositionFromIndex + 2 + currentIndexKey.remaining();
-                    dataSizeFromIndex = nextPartitionPositionFromIndex - dataStartFromIndex;
+                    if (currentIndexKey != null)
+                    {
+                        dataStartFromIndex = currentPartitionPositionFromIndex + 2 + currentIndexKey.remaining();
+                        dataSizeFromIndex = nextPartitionPositionFromIndex - dataStartFromIndex;
+                    }
                 }
+
+                long dataStart = dataFile.getFilePointer();
 
                 String keyName = key == null ? "(unreadable key)" : keyString(key);
                 outputHandler.debug(String.format("partition %s is %s", keyName, FBUtilities.prettyPrintMemory(dataSizeFromIndex)));
@@ -310,11 +325,8 @@ public class Scrubber implements Closeable
             }
 
             // finish obsoletes the old sstable
+            transaction.obsoleteOriginals();
             finished.addAll(writer.setRepairedAt(badPartitions > 0 ? ActiveRepairService.UNREPAIRED_SSTABLE : sstable.getSSTableMetadata().repairedAt).finish());
-        }
-        catch (IOException e)
-        {
-            throw Throwables.propagate(e);
         }
         finally
         {
@@ -402,6 +414,8 @@ public class Scrubber implements Closeable
             outputHandler.warn("Error reading index file", th);
             nextIndexKey = null;
             nextPartitionPositionFromIndex = dataFile.length();
+            if (indexFile != null)
+                indexFile.seek(indexFile.length());
         }
     }
 